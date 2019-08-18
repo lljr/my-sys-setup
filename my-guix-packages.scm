18199d5a4033d78959c79784b21dfb7f0dfdb4c9
jon <j@deb>
2019-08-17 22:35:35 -0600

    first

---
+++ my-guix-packages.scm
@@ -0,0 +1,36 @@
+(specifications->manifest
+ '("glibc-locales"
+
+   ;; Fonts
+   "gs-fonts" "font-dejavu" "font-gnu-freefont-ttf"
+
+   ;; Certs
+   "nss-certs"
+
+   ;; Apps
+   "emacs"
+   "gimp"
+   "inkscape"
+   "blender"
+   "icecat"
+
+   ;; Programs
+   "git"
+   "node"
+   "openjdk"
+   "guile"
+   "recutils"
+   "wget"
+   
+
+   ;; Guile
+   "artanis"
+   "haunt"
+
+   ;; Emacs
+   "emacs-guix"
+   "emacs-aggresive-indent"
+   "emacs-find-file-in-project"
+   "emacs-geiser"
+   "emacs-magit"
+   ))
